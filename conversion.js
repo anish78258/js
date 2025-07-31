cce02223f07f6d7602a6191e4a03f4e6858fbbb7
anish78258 <ak8850390@gmail.com>
2025-08-01 02:05:28 +0530

    coversion in js

---
+++ conversion.js
@@ -0,0 +1,13 @@
+let score = 33
+let newScore = "33abc"
+console.log(typeof score)
+let newString = Number(newScore)
+console.log( newString)
+let scoreString = String(score)
+console.log(scoreString)
+let valueScore = Number(scoreString)
+console.log(typeof valueScore)
+console.log(typeof NaN)
+
+// empty strng cnt to bool false 
+// when string has value cnt to bool true
