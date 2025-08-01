38f8e588ca956db8fbcdefbb6628384a5be011f1
anish78258 <ak8850390@gmail.com>
2025-08-02 02:04:49 +0530

    conversion in js

--- conversion.js
+++ conversion.js
@@ -11,3 +11,32 @@ console.log(typeof NaN)
 
 // empty strng cnt to bool false 
 // when string has value cnt to bool true
+
+//auto conversion by js
+// in plus "4" + 2 = "42"
+// in minus "4" - 2 = 2 
+// in multiplication "4" * 2 = 8
+// in division "4" / 2 = 2
+
+// in + , - left to right
+// in * , / , %left to right
+// 1. Operators are evaluated left to right (for +, -, *, etc.).
+// 2. If any operand of + is a string, JS switches to string concatenation from that point onward.
+// 3. -, *, / always convert operands to numbers.
+console.log(3 + 4 * 5 % 3)
+// first * % left to right // second + , - left to right
+console(+true)
+console(+"")
+let  num1 , num2 , num3;
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1 , num2 , num3)
+console.log("2" > 1)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+//note == and comparison is not same
+// in comparison null cnvt to 0 but not in ==
